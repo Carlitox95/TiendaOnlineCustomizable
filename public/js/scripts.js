b175bdab6c71c51fae6ea6dfb5462bb686ff13d9
CarlosVeron <43687202+Carlitox95@users.noreply.github.com>
2022-02-08 09:44:19 -0300

    Buscador de Productos

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -89,3 +89,39 @@ $(document).ready(function() {
  $('#tabla_filter').addClass("col s4");           
 });    
 
+//Funcion para quitar los acentos
+function quitarAcentos(cadena){
+ const acentos = {'á':'a','é':'e','í':'i','ó':'o','ú':'u','Á':'A','É':'E','Í':'I','Ó':'O','Ú':'U'};
+ return cadena.split('').map( letra => acentos[letra] || letra).join('').toString();    
+}
+
+//Funcion para buscar productos
+function buscadorProducto() {
+//Obtengo los productos
+productos= document.getElementsByName("productos");
+//Obtengo el valor ingresado en el input del buscador
+let valorIngresado=document.getElementById("parametroBusquedaProducto").value.toUpperCase();
+valorIngresado=quitarAcentos(valorIngresado);
+let coincidencias=0;
+    //Vamos a iterar 1 a 1 con todas las busquedas
+    for (let i = 0; i < productos.length; i++) {   
+     let nombreProducto=productos[i].getAttribute("productoNombre"); 
+        if (nombreProducto) {
+            if (nombreProducto.toUpperCase().indexOf(valorIngresado) > -1) {
+             //Si el parametro coincide con la busqueda lo dejo visible
+             productos[i].style.display="block";
+             coincidencias++;
+             //Quito el cartel de aviso
+             //document.getElementById("mensajeBuscador").innerHTML=null;
+            } 
+            else {
+             //Si no coincide con la busqueda oculto el contenido
+             productos[i].style.display="none";
+            }
+        }                               
+    }    
+    //Si la caja de tramites me queda vacia... no hay resultadas de la busqueda
+    if (coincidencias==0) {
+     //document.getElementById("mensajeBuscador").innerHTML="<strong><i class='material-icons'>error</i> No se encontraron resultados para la busqueda</strong>";
+    }      
+}
